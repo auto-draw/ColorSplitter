5175b80943c3838910abb037a83f2686a21ac29a
Siydge <53163523+Siydge@users.noreply.github.com>
2024-08-10 03:43:11 +1000

    Partial Port to AutoDraw UI

---
+++ Utils.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SkiaSharp;
+using Formatting = Newtonsoft.Json.Formatting;
+using Vector = Avalonia.Vector;
+
+namespace csp;
+
+public static class ImageExtensions
+{
+    public static Bitmap ConvertToAvaloniaBitmap(this SKBitmap bitmap)
+    {
+        return new Bitmap
+        (PixelFormat.Bgra8888, AlphaFormat.Premul,
+            bitmap.GetPixels(),
+            new PixelSize(bitmap.Width, bitmap.Height),
+            new Vector(96, 96),
+            bitmap.GetPixelSpan().Length
+        );
+    }
+}
+
+public class Config
+{
+    public static string FolderPath =
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "AutoDraw");
+
+    public static string ConfigPath = Path.Combine(FolderPath, "config.json");
+    public static string ThemesPath = Path.Combine(FolderPath, "Themes");
+    public static string CachePath = Path.Combine(FolderPath, "Cache");
+
+    public static void init()
+    {
+        Directory.CreateDirectory(FolderPath);
+        if (!File.Exists(ConfigPath))
+        {
+            JObject obj = new();
+            // Migrates old directory list path (from AutoDraw v1) to the new config file
+            var OldPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "AutoDraw");
+            if (File.Exists(Path.Combine(OldPath, "dir.txt")) &&
+                File.ReadAllText(Path.Combine(OldPath, "dir.txt")).Length != 0)
+                obj.Add("ConfigFolder", File.ReadAllText(Path.Combine(OldPath, "dir.txt")));
+            var emptyJObject = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(ConfigPath, emptyJObject);
+            
+        }
+
+        Utils.Copy(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Styles"), ThemesPath);
+        
+        // Check Configuration Path for Themes
+        if (GetEntry("SavedThemesPath") is null || !Directory.Exists(GetEntry("SavedPath")))
+        {
+            Directory.CreateDirectory(ThemesPath);
+            SetEntry("SavedThemesPath", ThemesPath);
+        }
+        else
+        {
+            ThemesPath = GetEntry("SavedThemesPath")!;
+        }
+        
+        // Check Configuration Path for Cache
+        if (GetEntry("SavedCachePath") is null || !Directory.Exists(GetEntry("SavedPath")))
+        {
+            Directory.CreateDirectory(CachePath);
+            SetEntry("SavedCachePath", CachePath);
+        }
+        else
+        {
+            CachePath = GetEntry("SavedCachePath")!;
+        }
+    }
+
+    public static string? GetEntry(string entry)
+    {
+        if (!File.Exists(ConfigPath)) return null;
+        var json = File.ReadAllText(ConfigPath);
+        var parse = JObject.Parse(json);
+        return (string?)parse[entry];
+    }
+
+    public static bool SetEntry(string entry, string data)
+    {
+        if (!File.Exists(ConfigPath)) return false;
+        var json = File.ReadAllText(ConfigPath);
+        var jsonFile = JObject.Parse(json);
+        jsonFile[entry] = data;
+        File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(jsonFile, Formatting.Indented));
+        return true;
+    }
+}
+
+public static class Utils
+{
+    public static string LogFolder = Path.Combine(Config.FolderPath, "logs");
+    public static string LogsPath = Path.Combine(LogFolder, $"{DateTime.Now:dd.MM.yyyy}.txt");
+    public static bool LoggingEnabled = Config.GetEntry("logsEnabled") == "True";
+    public static StreamWriter? LogObject;
+
+    public static unsafe SKBitmap NormalizeColor(this SKBitmap SourceBitmap)
+    {
+        var srcColor = SourceBitmap.ColorType;
+        var srcAlpha = SourceBitmap.AlphaType;
+
+        if (srcColor == SKColorType.Bgra8888) return SourceBitmap;
+        // Ensure we don't need to normalize it.
+
+        SKBitmap OutputBitmap = new(SourceBitmap.Width, SourceBitmap.Height);
+
+        var srcPtr = (byte*)SourceBitmap.GetPixels().ToPointer();
+        var dstPtr = (byte*)OutputBitmap.GetPixels().ToPointer();
+
+        var width = OutputBitmap.Width;
+        var height = OutputBitmap.Height;
+
+        var outColor = OutputBitmap.ColorType;
+
+        for (var row = 0; row < height; row++)
+        for (var col = 0; col < width; col++)
+            if (srcColor == SKColorType.Gray8 || srcColor == SKColorType.Alpha8)
+            {
+                var b = *srcPtr++;
+                *dstPtr++ = b;
+                *dstPtr++ = b;
+                *dstPtr++ = b;
+                *dstPtr++ = 255;
+            }
+            else if (srcColor == SKColorType.Rgba8888)
+            {
+                var r = *srcPtr++;
+                var g = *srcPtr++;
+                var b = *srcPtr++;
+                var a = *srcPtr++;
+                *dstPtr++ = b;
+                *dstPtr++ = g;
+                *dstPtr++ = r;
+                *dstPtr++ = a;
+            }
+            else if (srcColor == SKColorType.Argb4444)
+            {
+                var r = *srcPtr++;
+                var g = *srcPtr++;
+                var b = *srcPtr++;
+                var a = *srcPtr++;
+                *dstPtr++ = (byte)(b * 2);
+                *dstPtr++ = (byte)(g * 2);
+                *dstPtr++ = (byte)(r * 2);
+                *dstPtr++ = (byte)(a * 2);
+            }
+
+        SourceBitmap.Dispose();
+        SourceBitmap = OutputBitmap;
+
+        return SourceBitmap;
+    }
+    
+    public static void Log(string text)
+    {
+        Debug.WriteLine(text);
+        if (!LoggingEnabled) return;
+        if (!Directory.Exists(LogFolder)) Directory.CreateDirectory(LogFolder);
+        if (LogObject == null) LogObject = new StreamWriter(LogsPath);
+        LogObject.WriteLine(text);
+        LogObject.Flush();
+    }
+
+    public static void Copy(string sourceDirectory, string targetDirectory)
+    {
+        var diSource = new DirectoryInfo(sourceDirectory);
+        var diTarget = new DirectoryInfo(targetDirectory);
+        CopyAll(diSource, diTarget);
+    }
+
+    public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+    {
+        Directory.CreateDirectory(target.FullName);
+
+        // Copy each file into the new directory.
+        foreach (var fi in source.GetFiles())
+        {
+            Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+            fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+        }
+
+        // Copy each subdirectory using recursion.
+        foreach (var diSourceSubDir in source.GetDirectories())
+        {
+            var nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+            CopyAll(diSourceSubDir, nextTargetSubDir);
+        }
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,27 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using SkiaSharp;
 
 namespace csp.Views;
 
 public partial class MainWindow : Window
 {
+    private SKBitmap _rawBitmap;
+    private Bitmap _previewBitmap;
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ImportImage(string? path, byte[]? img = null)
+    {
+        _rawBitmap = img is null ? SKBitmap.Decode(path).NormalizeColor() : SKBitmap.Decode(img).NormalizeColor();
+
+        _previewBitmap = _rawBitmap.ConvertToAvaloniaBitmap();
+        
+        ImagePreview.Image = _previewBitmap;
     }
 }

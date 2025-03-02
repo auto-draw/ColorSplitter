40ac80bde7a60bd0638bf2137c8d4d39276ef0be
Siydge <53163523+Siydge@users.noreply.github.com>
2025-03-02 23:20:06 +1100

    Fix build script

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ read -p "Please enter your choice: " choice
 if [ $choice == "1" ] || [ $choice == "6" ]
 then
 	echo "Building for Windows x64"
-	dotnet publish \csp.csproj -r win-x64 -c Release -p:publishsinglefile=true --self-contained false -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-x64-$buildExtension/csp
+	dotnet publish \ColorSplitter.csproj -r win-x64 -c Release -p:publishsinglefile=true --self-contained false -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-x64-$buildExtension/csp
 	cd Builds/colorsplitter-win-x64-$buildExtension
 	zip -r $OLDPWD/Builds/colorsplitter-win-x64-$buildExtension.zip .
 	cd -
@@ -29,7 +29,7 @@ fi
 if [ $choice == "2" ] || [ $choice == "6" ]
 then
 	echo "Building for Windows x86"
-	dotnet publish \csp.csproj -r win-x86 -c Release -p:publishsinglefile=true --self-contained false -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-x86-$buildExtension/csp
+	dotnet publish \ColorSplitter.csproj -r win-x86 -c Release -p:publishsinglefile=true --self-contained false -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-x86-$buildExtension/csp
 	cd Builds/colorsplitter-win-x86-$buildExtension
 	zip -r $OLDPWD/Builds/colorsplitter-win-x86-$buildExtension.zip .
 	cd -
@@ -38,7 +38,7 @@ fi
 if [ $choice == "3" ] || [ $choice == "6" ]
 then
 	echo "Building for Windows Selfcontained-x64"
-	dotnet publish \csp.csproj -r win-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-$buildExtension/csp
+	dotnet publish \ColorSplitter.csproj -r win-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-win-$buildExtension/csp
 	cd Builds/colorsplitter-win-$buildExtension
 	zip -r $OLDPWD/Builds/colorsplitter-win-$buildExtension.zip .
 	cd -
@@ -47,7 +47,7 @@ fi
 if [ $choice == "4" ] || [ $choice == "6" ]
 then
 	echo "Building for MacOS x64"
-	dotnet publish \csp.csproj -r osx-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-macos-x64-$buildExtension/csp
+	dotnet publish \ColorSplitter.csproj -r osx-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-macos-x64-$buildExtension/csp
 	cd Builds/colorsplitter-macos-x64-$buildExtension
 	zip -r $OLDPWD/Builds/colorsplitter-macos-x64-$buildExtension.zip .
 	cd -
@@ -56,7 +56,7 @@ fi
 if [ $choice == "5" ] || [ $choice == "6" ]
 then
 	echo "Building for Linux x64"
-	dotnet publish \csp.csproj -r linux-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-linux-x64-$buildExtension/csp
+	dotnet publish \ColorSplitter.csproj -r linux-x64 -c Release -p:publishsinglefile=true --self-contained true -p:debugsymbols=false -p:debugtype=none -o Builds/colorsplitter-linux-x64-$buildExtension/csp
 	cd Builds/colorsplitter-linux-x64-$buildExtension
 	zip -r $OLDPWD/Builds/colorsplitter-linux-x64-$buildExtension.zip .
 	cd -
